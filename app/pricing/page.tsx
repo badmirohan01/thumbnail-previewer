44dcd22ff9e61166ad84b47a3462ed72cbdbce8e
badmirohan01 <rohanbadmi@gmail.com>
2025-08-27 22:40:10 +0530

    User interface setup

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,11 +4,11 @@ const isSubscribed = true;
 export default async function Pricing() {
     return (
         <div className="max-w-2xl mx-auto py-8">
-            <h1>Subscription Plan</h1>
-            <div>
-                <h2>Full Access</h2>
-                <p>Access to all features</p>
-                <p>7299/month</p>
+            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Subscription Plan</h1>
+            <div className="space-y-4 rounded-lg border bg-card text-card-foreground shadow-sm p-6">
+                <h2 className="border-b pb-2 text-3xl font-semibold tracking-tight">Full Access</h2>
+                <p className="leading-7">Access to all features</p>
+                <p className="text-2xl font-bold">7299/month</p>
                 {isSubscribed ? (
                     <form action="">
                         <Button type="submit">Manage Subscription</Button>

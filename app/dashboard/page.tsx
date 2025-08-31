f80e2bffdad524c04091f51fed7c2bc2c18d70f0
badmirohan01 <rohanbadmi@gmail.com>
2025-08-31 10:39:20 +0530

    Dashboard component setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import ThumbnailPreviewer from "./_components/thumbnail-previewer";
 
 export default async function Dashboard() {
     const { userId } = await auth();
@@ -28,11 +29,13 @@ export default async function Dashboard() {
             </div>
         )
     }
+    const channelName = "life_guru"
 
     return (
         <>
             <div className="flex flex-col items-center justify-center p-8 rounded-lg shadow-md">
                 <p className="text-xl mb-6">You are Subscribed and good to go</p>
+                <ThumbnailPreviewer channelNameSaved={channelName} />
             </div>
         </>
     );
